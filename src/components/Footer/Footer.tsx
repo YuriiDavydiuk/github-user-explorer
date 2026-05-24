8ead7cb190f31ddac3ef34f660044fa979b40b4d
Yuri <davydiuk.yurii@gmail.com>
2026-05-24 21:42:43 +0300

    fix: change CitHub to GitHub in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import css from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={css.footer}>
-      <p>CitHub User Explorer {new Date().getFullYear()}</p>
+      <p>GitHub User Explorer {new Date().getFullYear()}</p>
     </footer>
   );
 }
